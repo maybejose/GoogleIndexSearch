7e4a09a1c71d14e791ad8c6426c1d303e53db4b5
maybejose <maybejose@gmail.com>
2017-08-03 13:57:27 -0400

    Add files via upload

---
+++ google_index_search.py
@@ -0,0 +1,37 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import time
+
+def search():
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    start = 0
+    searchword = input('Enter the word(s) to search for: ')
+    if ' ' in searchword: searchword = searchword.split()
+    searchaddress = input('Enter the address to search for: ')
+    url = 'http://www.google.com/search?q='+'+'.join(searchword)
+    position = 0
+    while True:
+        print('Retrieving',url)
+        print('Checking from article',start+1)
+        try:
+            req = Request(url, headers=headers)
+            html = urlopen(req).read()
+        except Exception as e:
+            print(str(e))
+            return
+        soup = BeautifulSoup(html, "html.parser")
+        tags = soup('cite')
+        for tag in tags:
+            position += 1
+            if searchaddress in str(tag):
+                print('Address found on:')
+                print(url)
+                print('in position',position,'of Google.com index')
+                return
+        start += 10
+        url = url +'&start='+str(start)
+        time.sleep(3)
+
+if __name__ == '__main__':
+    search()
